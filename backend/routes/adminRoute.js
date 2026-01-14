839f79451bc36bf62ec6638b596c1e09d21b14e6
omprakash-06 <omprkashnagesh@gmail.com>
2026-01-15 01:04:14 +0530

    Remove admin registration route
    Removed the registration route for admins.

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { registerAdmin, loginAdmin, getAdminProfile, logoutAdmin } = require("../controllers/AdminController");
+const { loginAdmin, getAdminProfile, logoutAdmin } = require("../controllers/AdminController");
 const { verifyAdmin } = require("../middleware/verifyAdmin");
 
 const router = express.Router();
 
-router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
 
 // cookie-based protected route
